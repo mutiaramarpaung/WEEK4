d241e6e08ef0bcbed4c3fdb24eac0ef815690528
Mutiara Ferawati Marpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-04-12 11:28:15 +0700

    Add files via upload

---
+++ ubahHuruf.js
@@ -0,0 +1,20 @@
+/** UBAH HURUF */
+function ubahHuruf(kata) {
+  var abjad = 'abcdefghijklmnopqrstuvwxyz';
+  var ubah = '';
+  for (var i = 0; i < kata.length; i++) {
+    for (var j = 0; j < abjad.length; j++) {
+      if (kata[i] === abjad[j]) {
+        res += abjad[j+1]
+      }
+    }
+  }
+  return ubah;
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
