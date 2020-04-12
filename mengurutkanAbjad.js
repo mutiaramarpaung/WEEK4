535bf6af356e67f93d885edd555be8c4acafbe25
Mutiara Ferawati Marpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-04-12 11:50:13 +0700

    Update mengurutkanAbjad.js

--- mengurutkanAbjad.js
+++ mengurutkanAbjad.js
@@ -14,8 +14,8 @@ function urutkanAbjad(str) {
 }
 
 // TEST CASES
-// console.log(urutkanAbjad('hello')); // 'ehllo'
-// console.log(urutkanAbjad('truncate')); // 'acenrttu'
-// console.log(urutkanAbjad('developer')); // 'deeeloprv'
-// console.log(urutkanAbjad('software')); // 'aeforstw'
-// console.log(urutkanAbjad('aegis')); // 'aegis'
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
