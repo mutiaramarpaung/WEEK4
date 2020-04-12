78d201068f8d9461bf4366564b697d5a61ac6847
Mutiara Ferawati Marpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-04-12 10:40:09 +0700

    Add files via upload

---
+++ faktorPersekutuanTerbesarA2.js
@@ -0,0 +1,16 @@
+/** FAKTOR FPB*/
+
+function fpb(angka1, angka2) {
+  for (var x=angka1; x>0; x--){
+    if(angka2 % x === 0 && angka1 % x===0){
+      return x;
+    }
+  }
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
