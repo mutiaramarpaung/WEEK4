53428cd63243bd365a041753267041a8d146ab89
Mutiara Ferawati Marpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-04-12 11:35:31 +0700

    Add files via upload

---
+++ mengurutkanAbjad.js
@@ -0,0 +1,21 @@
+/** URUTKAN ABJAD */ 
+function urutkanAbjad(str) {
+   var abjad = 'abcdefghijklmnopqrstuvwxyz', urutan = '';
+  
+   for (var i = 0; i < abjad.length; i++) {
+     for (var j = 0; j < str.length; j++) {
+       if (abjad[i] === str[j]) {
+         urutan += abjad[i];
+       }
+     }
+   }
+  
+   return urutan;
+}
+
+// TEST CASES
+// console.log(urutkanAbjad('hello')); // 'ehllo'
+// console.log(urutkanAbjad('truncate')); // 'acenrttu'
+// console.log(urutkanAbjad('developer')); // 'deeeloprv'
+// console.log(urutkanAbjad('software')); // 'aeforstw'
+// console.log(urutkanAbjad('aegis')); // 'aegis'
